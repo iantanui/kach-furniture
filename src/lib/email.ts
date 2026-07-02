d0cb85b8aeb7c3ab1c6dffba138bdb37abe6584b
iantanui <rutooian@gmail.com>
2026-07-02 16:05:19 +0300

    edit name

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEnquiryEmail(data: EnquiryFormValues) {
   await resend.emails.send({
-    from: "Furniture Shop <onboarding@resend.dev>",
+    from: "Alkosphere Furniture Shop <onboarding@resend.dev>",
     to: process.env.SHOP_NOTIFICATION_EMAIL!,
     subject: `New Enquiry from ${data.name}`,
     html: `
